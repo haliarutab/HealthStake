21c762b56d869c618b244aa1c8d14c6b3a541519
HALIA RUTAB <haliarutab24@gmail.com>
2025-11-21 12:33:52 +0500

    WhychooseUs

--- src/app/home/Homepage.tsx
+++ src/app/home/Homepage.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import Navbar from "@/components/navbar/Navbar";
-
+import Hero from "@/components/herosection/herosection";
+import WhyChooseUs from "@/components/whychooseus/whychooseus";
+import HowPlatformWorks from "@/components/HowPlatformWorks/howplatformworks";
 export default function HomePage() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <WhyChooseUs />
+      <HowPlatformWorks />
       {/* Add more sections later */}
     </div>
   );
